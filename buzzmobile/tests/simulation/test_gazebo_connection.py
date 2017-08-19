9cb2abec26da201e25f66b9c2cfff6e6fad22cbd
Joshua Morton <joshua.morton13@gmail.com>
2017-08-19 18:01:40 -0400

    New way of running the simulation test.
    Sahit, please try running ` roslaunch buzzmobile simulation_test.launch`
    locally and seeing what happens. I get errors that the controller
    manager cannot find dependencies, and I want to see if that's true for
    you as well.
    
    I think this implies some issues in either the urdf or in
    sim_car_interface.cpp, but I'm not 100% sure of that.

--- buzzmobile/tests/simulation/test_gazebo_connection.py
+++ buzzmobile/tests/simulation/test_gazebo_connection.py
@@ -1,17 +1,20 @@
-from pyrostest import RosTest, with_launch_file
-from rosgraph_msgs.msg import Clock
+"""Simple simulation test to connect to Gazebo."""
+
 import time
+
+from rosgraph_msgs.msg import Clock
+
+from pyrostest import RosTest, with_launch_file
 
 class TestGazeboConnection(RosTest):
     """Tests for simulation testing infrastructure."""
 
-    @with_launch_file('buzzmobile', 'simulation.launch')
+    @with_launch_file('buzzmobile', 'simulation_test.launch')
     def test_clock_runs(self):
         """Test that simulation.launch correctly launches gazebo"""
         with self.check_topic('/clock', Clock) as ct:
             assert(ct.message)
-            print(ct.message)
-        time.sleep(30)
-        with self.check_topic('/clock', Clock, 20) as ct1:
-            print(ct1.message)
+
+        time.sleep(.1)
+        with self.check_topic('/clock', Clock, timeout=20) as ct1:
             assert(ct1.message)
