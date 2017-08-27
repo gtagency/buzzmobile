2ef361a13c8fe86ebc52e49913718b299721889e
Joshua Morton <joshua.morton13@gmail.com>
2017-08-27 17:31:26 -0400

    Just for testing porpoises.

--- buzzmobile/tests/unit/plan/test_controller.py
+++ buzzmobile/tests/unit/plan/test_controller.py
@@ -5,13 +5,3 @@ from sensor_msgs.msg import Joy
 from buzzmobile.msg import CarPose
 from buzzmobile.msg import CarState
 
-class TestCarState(RosTest):
-
-    @with_launch_file('buzzmobile', 'test_params.launch')
-    @launch_node('buzzmobile', 'controller')
-    def test_state(self):
-        with self.mock_pub('/joy', Joy, queue_size=0) as joy_node:
-            with self.check_topic('/buzzmobile/car_state', CarState) as cs:
-                # Ensure starts in Start Mode
-                assert True
-                # TODO(cole): finish writing test
